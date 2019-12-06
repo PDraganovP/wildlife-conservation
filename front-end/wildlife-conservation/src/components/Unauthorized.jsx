f28260854406c3ebd00e6efe5be998db00c45403
PDraganovP <petar.draganov.petrov@abv.bg>
2019-12-06 14:37:51 +0200

    Added new component

---
+++ front-end/wildlife-conservation/src/components/Unauthorized.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Unauthorized = () => {
+    return (
+        <div>
+            <h1 className="text-center">You are unauthorized</h1>
+        </div>
+    )
+}
+
+export default Unauthorized
